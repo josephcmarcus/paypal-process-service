42a2beaf657500beb54f7af47c5d1d7225462ba5
Joe Marcus <josephcmarcus@gmail.com>
2022-12-20 15:54:05 -0500

    Wrap inputs into array to pass to processRecords function

--- paypal-process-service/index.js
+++ paypal-process-service/index.js
@@ -1,13 +1,14 @@
-﻿const df = require("durable-functions");
+﻿const df = require('durable-functions');
 
 module.exports = df.orchestrator(function* (context) {
+  const inputs = [];
   const outputs = [];
 
-  const paypalToken = yield context.df.callActivity("getAccessToken", null);
-  const records = yield context.df.callActivity("getRecords", null);
-  const results = yield context.df.callActivity("processRecords", records);
+  const paypalToken = yield context.df.callActivity('getAccessToken', null);
+  const records = yield context.df.callActivity('getRecords', null);
+  inputs.push(paypalToken, records);
+  const results = yield context.df.callActivity('processRecords', inputs);
 
   outputs.push(paypalToken, records, results);
   return outputs;
-
 });
