d9c480cbf0a959429dbfea18b54010ce6aded617
Joe Marcus <josephcmarcus@gmail.com>
2023-01-04 16:44:06 -0500

    Add more detail to log messages for getAccessToken function

--- getAccessToken/index.js
+++ getAccessToken/index.js
@@ -2,27 +2,34 @@
 const axios = require('axios');
 
 module.exports = async function (context) {
-  context.log('Attempting to obtain access token from PayPal');
+  const { instanceId } = context.bindingData.args;
 
-  const response = await axios({
-    method: 'post',
-    url: `${process.env.PAYPAL_BASE_URL}/v1/oauth2/token`,
-    data: {
-      grant_type: 'client_credentials',
-      ignoreCache: true,
-    },
-    headers: {
-      Accept: '*/*',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept-Language': 'en_US',
-    },
-    auth: {
-      username: process.env.PAYPAL_CLIENT_ID,
-      password: process.env.PAYPAL_SECRET,
-    },
-  });
-
-  context.log('Successfully obtained access token from PayPal');
-
-  return response.data.access_token;
+  try {
+    context.log(`Starting getAccessToken Paypal API query for instance = '${instanceId}'.`);
+    
+    const response = await axios({
+      method: 'post',
+      url: `${process.env.PAYPAL_BASE_URL}/v1/oauth2/token`,
+      data: {
+        grant_type: 'client_credentials',
+        ignoreCache: true,
+      },
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept-Language': 'en_US',
+      },
+      auth: {
+        username: process.env.PAYPAL_CLIENT_ID,
+        password: process.env.PAYPAL_SECRET,
+      },
+    });
+  
+    context.log(`getAccessToken succeeded for ID = '${instanceId}'.`)
+  
+    return response.data.access_token;
+  } catch (err) {
+    context.log(`getAccessToken failed for ID = '${instanceId}'. ${err}`)
+    return null;
+  }
 };
