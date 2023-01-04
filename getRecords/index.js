d70519bef25605b8262abd8ef3ebd2f37cd874d4
Joe Marcus <josephcmarcus@gmail.com>
2023-01-04 16:09:15 -0500

    Add conditional check to end function if no valid records are received

--- getRecords/index.js
+++ getRecords/index.js
@@ -1,14 +1,23 @@
 ﻿const database = require('../database')
 
+/* In either this file or elsewhere, need to refine the the way we're grabbing records
+from the database. As it stands right now, we'd be grabbing every record from the table.
+Instead, we need to only grab relevant records to process – might need to add another
+column to the database table or create an advanced query to only pull relevant records. */
+
 module.exports = async function (context) {
+    const { instanceId } = context.bindingData.args;
+
     try {
-        context.log('Attempting to obtain records from database')
+        context.log(`Starting getRecords database query for instance = '${instanceId}'.`);
+
         const table = process.env.DB_TEST_TABLE;
-        context.log('Received records from database')
-        return await database.getRecords(table);
+        const records = await database.getRecords(table);
+
+        context.log(`getRecords succeeded for ID = '${instanceId}'.`);
+        return records;
     } catch (err) {
-        context.log(`Error obtaining records from database: ${err}`)
+        context.log(`getRecords failed for ID = '${instanceId}'. ${err}`);
+        return null;
     }
-    const table = process.env.DB_TEST_TABLE;
-    return await database.getRecords(table);
 };
--- paypal-process-service/index.js
+++ paypal-process-service/index.js
@@ -1,14 +1,41 @@
 ﻿const df = require('durable-functions');
 
 module.exports = df.orchestrator(function* (context) {
-  const inputs = [];
+  const activityPayload = {
+    instanceId: context.df.instanceId,
+    paypalToken: '',
+    records: [],
+  };
   const outputs = [];
 
-  const paypalToken = yield context.df.callActivity('getAccessToken', null);
-  const records = yield context.df.callActivity('getRecords', null);
-  inputs.push(paypalToken, records);
-  const results = yield context.df.callActivity('processRecords', inputs);
+  const paypalToken = yield context.df.callActivity('getAccessToken', activityPayload);
+  if (paypalToken === null) {
+    const message = `Could not process records for ID = '${activityPayload.instanceId}'. An error occurred in the getAccessToken function.`;
+    context.log(message)
+    return message;
+  }
+  activityPayload.paypalToken = paypalToken;
+
+  const records = yield context.df.callActivity('getRecords', activityPayload);
+  if (records === null) {
+    const message = `Could not process records for ID = '${activityPayload.instanceId}'. An error occurred in the getRecords function.`;
+    context.log(message);
+    return message;
+  } else if (records.length === 0) {
+      const message = `No valid records to process for ID = '${activityPayload.instanceId}'. Exiting function.`;
+      context.log(message);
+      return message;
+  }
+  activityPayload.records = records;
+
+  // change processRecordsTest back to processRecords when finished testing
+  const results = yield context.df.callActivity('processRecordsTest', activityPayload);
 
   outputs.push(records, results);
   return outputs;
 });
+
+
+// update getRecords with new SQL logic to only pull records without a process date
+// update processRecords to write new SQL date value for each record processed
+// add better context logging to processRecords
