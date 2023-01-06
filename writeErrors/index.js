a0a331018b56a83612fe628d443e1bc1735d8afc
Joe Marcus <josephcmarcus@gmail.com>
2023-01-06 09:47:36 -0500

    Create writeErrors function to pass errors from processRecords to database writeErrors function

---
+++ writeErrors/index.js
@@ -0,0 +1,21 @@
+﻿const database = require('../database');
+
+module.exports = async function (context) {
+  const { errors, instanceId } = context.bindingData.args;
+  const errorValues = [];
+  const table = process.env.DB_ERROR_TABLE;
+  const columns = `Billing_Agreement, PPStagingID, InstanceID, Error, Trace, Date`;
+
+  for (error of errors) {
+    errorValues.push(Object.values(error));
+  }
+
+  try {
+    await database.writeErrors(table, columns, errorValues);
+    context.log(`writeErrors succeeded to update database for instance = '${instanceId}'.`);
+  } catch (err) {
+    context.log(`writeErrors failed to update database for instance = '${instanceId}'. ${err}`);
+  }
+  
+  return errorValues;
+};
