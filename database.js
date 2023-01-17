0ac2bc16f5bdde502269af38e65afa2fbeb8cbb2
Joe Marcus <josephcmarcus@gmail.com>
2023-01-17 13:58:04 -0500

    Remove unnecessary comments

--- database.js
+++ database.js
@@ -2,22 +2,6 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const fs = require('fs');
 
-// create the connection to the database without using pool
-// const connection = await mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PW,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   ssl: {
-//     ca: fs.readFileSync(__dirname + process.env.CRT),
-//   },
-// });
-
-// query the database using single connection
-// const [rows] = await connection.execute(`SELECT * FROM ${table}`);
-// return rows;
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
